dfbb3b79f74ac01bf27bd181a6d27a385b86e85c
xylo <devyarustagi@gmail.com>
2026-06-09 02:38:45 +0530

    feat: add cors middleware

--- internal/router/router.go
+++ internal/router/router.go
@@ -1,12 +1,22 @@
 package router
 
 import (
+	"os"
 	"github.com/devyarustagi/Politique/internal/handlers"
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 func RouterSetup(pool *pgxpool.Pool) *chi.Mux{
 	r:= chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+    AllowedOrigins:   []string{os.Getenv("FRONTEND_URL")},
+    AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+    AllowedHeaders:   []string{"Content-Type"},
+	AllowCredentials: true,
+	}))
+
 	handler:= handlers.New(pool)
 	r.Post("/api/auth/register", handler.Register)
 	r.Post("/api/auth/login", handler.Login)
